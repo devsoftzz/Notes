6f8802b891973731f1c6a20def50c7154c926c72
devsoftzz <devsoft.deven@gmail.com>
2019-12-26 00:26:07 +0530

    UI Part Done

--- app/src/main/java/com/devsoftzz/notes/MainActivity.java
+++ app/src/main/java/com/devsoftzz/notes/MainActivity.java
@@ -1,7 +1,10 @@
 package com.devsoftzz.notes;
 
+import androidx.annotation.MainThread;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -9,11 +12,13 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.devsoftzz.notes.Adapters.NotesRecyclerAdapter;
 import com.devsoftzz.notes.Models.note;
 import com.devsoftzz.notes.Util.RecyclerDecore;
+import com.google.android.material.button.MaterialButton;
 
 import java.util.ArrayList;
 
@@ -33,6 +38,13 @@ public class MainActivity extends AppCompatActivity implements NotesRecyclerAdap
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
         setTitle("Notes");
         mRecyclerView = findViewById(R.id.recyclerView);
+
+        findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,NoteActivity.class));
+            }
+        });
 
         initRecyclerView();
         insertFakeNotes();
@@ -55,16 +67,33 @@ public class MainActivity extends AppCompatActivity implements NotesRecyclerAdap
         mRecyclerView.setLayoutManager(linearLayoutManager);
         RecyclerDecore recyclerDecore = new RecyclerDecore(12);
         mRecyclerView.addItemDecoration(recyclerDecore);
+        new ItemTouchHelper(simpleCallback).attachToRecyclerView(mRecyclerView);
         mNotesRecyclerAdapter = new NotesRecyclerAdapter(mNotes,this,this);
         mRecyclerView.setAdapter(mNotesRecyclerAdapter);
     }
 
     @Override
     public void onNoteClickListner(int position) {
-
         Log.d(TAG, "onNoteClickListner: "+position);
         Intent intent = new Intent(this,NoteActivity.class);
         intent.putExtra("SelectedNote",mNotes.get(position));
         startActivity(intent);
+    }
+
+    private ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.RIGHT) {
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+            deleteNote(viewHolder.getAdapterPosition());
+        }
+    };
+
+    private void deleteNote(int index) {
+        mNotes.remove(index);
+        mNotesRecyclerAdapter.notifyItemRemoved(index);
     }
 }
--- app/src/main/java/com/devsoftzz/notes/NoteActivity.java
+++ app/src/main/java/com/devsoftzz/notes/NoteActivity.java
@@ -14,19 +14,14 @@ import android.widget.TextView;
 import com.devsoftzz.notes.Models.note;
 import com.devsoftzz.notes.Util.EditTextWithLines;
 
-public class NoteActivity extends AppCompatActivity implements
-        View.OnTouchListener,
-        GestureDetector.OnGestureListener,
-        GestureDetector.OnDoubleTapListener {
+public class NoteActivity extends AppCompatActivity{
 
     private static final String TAG = "Notes";
     private note mNote;
-    private ImageButton mBack,mEdit;
-    private TextView mTitle,mMessage;
+    private ImageButton mBack;
     private EditTextWithLines mText;
-    private EditText mEditTitle;
+    private EditText mTitle;
     private boolean NewNote;
-    private GestureDetector mGestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,99 +29,26 @@ public class NoteActivity extends AppCompatActivity implements
         setContentView(R.layout.activity_note);
 
         mBack = findViewById(R.id.backBtn);
-        mEdit = findViewById(R.id.checkBtn);
-        mTitle = findViewById(R.id.textview);
         mText = findViewById(R.id.edittext);
-        mMessage = findViewById(R.id.editmessage);
-        mEditTitle = findViewById(R.id.editview);
+        mTitle = findViewById(R.id.editview);
 
-        if(isNewNote()){
-            editConfig();
+        if(!getIntent().hasExtra("SelectedNote")){
+            NewNote = true;
         }else {
-            viewCongig();
-        }
-
-        mText.setOnTouchListener(this);
-        mGestureDetector = new GestureDetector(this,this);
-    }
-
-    private void viewCongig() {
-        mMessage.setVisibility(View.VISIBLE);
-        mBack.setVisibility(View.VISIBLE);
-        mEdit.setVisibility(View.GONE);
-        mEditTitle.setVisibility(View.GONE);
-        mTitle.setVisibility(View.VISIBLE);
-        mText.setText(mNote.getContent());
-        mTitle.setText(mNote.getTitle());
-    }
-
-    private void editConfig() {
-        mMessage.setVisibility(View.GONE);
-        mBack.setVisibility(View.GONE);
-        mEdit.setVisibility(View.VISIBLE);
-        mEditTitle.setVisibility(View.VISIBLE);
-        mTitle.setVisibility(View.GONE);
-        if(NewNote){
-            mEditTitle.setText("Note Title");
-            mText.setText("");
-        }else {
-            mEditTitle.setText(mNote.getTitle());
-            mText.setText(mNote.getContent());
-        }
-    }
-
-    private boolean isNewNote(){
-        if(getIntent().hasExtra("SelectedNote")){
-            mNote = getIntent().getParcelableExtra("SelectedNote");
             NewNote = false;
-            return false;
+            mNote = getIntent().getParcelableExtra("SelectedNote");
+            mText.setText(mNote.getContent());
+            mTitle.setText(mNote.getTitle());
         }
-        NewNote = true;
-        return true;
-    }
 
-
-
-    @Override
-    public boolean onDoubleTap(MotionEvent e) {
-        editConfig();
-        return false;
-    }
-
-    @Override
-    public void onLongPress(MotionEvent e) {
-
-    }
-    @Override
-    public boolean onDoubleTapEvent(MotionEvent e) {
-        return false;
-    }
-    @Override
-    public boolean onDown(MotionEvent e) {
-        return false;
-    }
-    @Override
-    public void onShowPress(MotionEvent e) {
-
-    }
-    @Override
-    public boolean onSingleTapUp(MotionEvent e) {
-        return false;
-    }
-    @Override
-    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-        return false;
-    }
-    @Override
-    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-        return false;
-    }
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        return mGestureDetector.onTouchEvent(event);
-    }
-    @Override
-    public boolean onSingleTapConfirmed(MotionEvent e) {
-        return false;
+        mBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO
+                //Add Data To Database / Update Data
+                //Go Back
+                finish();
+            }
+        });
     }
 }
